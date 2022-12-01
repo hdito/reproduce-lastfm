4aadc6183d7c3a88637cf2f9577da63f8a55113f
hdito <hditow@gmail.com>
2022-12-02 00:24:19 +0300

    Add interactive search

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,23 +1,31 @@
+import { useState } from "react";
 import { FiSearch } from "react-icons/fi";
 import { HiMenu } from "react-icons/hi";
-import { IoPersonCircle } from "react-icons/io5";
+import { IoClose, IoPersonCircle } from "react-icons/io5";
 import { headerContent } from "../content/headerContent";
 import Logo from "/public/logoLastFm.png";
 
 export const Header = () => {
+  const [isOpenSearch, setIsOpenSearch] = useState(false);
   return (
     <header className="relative flex h-12 items-center gap-4 bg-black px-4 text-2xl text-white">
       <button className="lg:hidden">
         <HiMenu />
       </button>
-      <a href="#" className="lg:hidden">
-        <IoPersonCircle />
-      </a>
+      {!isOpenSearch ? (
+        <a href="#" className="lg:hidden">
+          <IoPersonCircle />
+        </a>
+      ) : null}
+
       <div className="absolute left-1/2 h-full -translate-x-1/2 p-3">
         <img className="block" src={Logo} alt="Las.fm logo" />
       </div>
-      <button className="ml-auto">
-        <FiSearch />
+      <button
+        onClick={() => setIsOpenSearch((prev) => !prev)}
+        className="ml-auto"
+      >
+        {!isOpenSearch ? <FiSearch /> : <IoClose />}
       </button>
       {headerContent.map((link) => (
         <a
@@ -30,6 +38,18 @@ export const Header = () => {
       <a href="#" className="hidden text-3xl lg:inline-block">
         <IoPersonCircle />
       </a>
+      {isOpenSearch ? (
+        <form className="absolute bottom-0 right-0 left-0 z-10 flex translate-y-full items-center gap-4 border-t border-lastfm-mineshaft bg-black">
+          <input
+            className="flex-1 bg-transparent px-4 py-1 text-base focus:outline-0"
+            placeholder="Search for music..."
+            type="search"
+          />
+          <button className="p-4">
+            <FiSearch />
+          </button>
+        </form>
+      ) : null}
     </header>
   );
 };
