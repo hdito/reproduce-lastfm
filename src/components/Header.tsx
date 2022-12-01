b0bf6f3ab5e67ab8a8088538347e7f941b7facbe
hdito <hditow@gmail.com>
2022-12-02 00:51:21 +0300

    Add music player to header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,14 +1,38 @@
 import { useState } from "react";
 import { FiSearch } from "react-icons/fi";
 import { HiMenu } from "react-icons/hi";
-import { IoClose, IoPersonCircle } from "react-icons/io5";
+import {
+  IoClose,
+  IoPauseCircleOutline,
+  IoPersonCircle,
+  IoPlayBackSharp,
+  IoPlayForwardSharp,
+} from "react-icons/io5";
+import { FaRecordVinyl, FaRegHeart } from "react-icons/fa";
 import { headerContent } from "../content/headerContent";
 import Logo from "/public/logoLastFm.png";
 
 export const Header = () => {
   const [isOpenSearch, setIsOpenSearch] = useState(false);
   return (
-    <header className="relative flex h-12 items-center gap-4 bg-black px-4 text-2xl text-white">
+    <header className="relative flex h-12 items-center gap-4 bg-black px-4 text-2xl text-white lg:h-fit lg:px-0 lg:pr-4">
+      <div className="hidden gap-1 text-2xl text-lastfm-graphite lg:flex ">
+        <button className="mr-8 bg-lastfm-mineshaft p-3 text-4xl">
+          <FaRecordVinyl />
+        </button>
+        <button>
+          <IoPlayBackSharp />
+        </button>
+        <button className="text-4xl">
+          <IoPauseCircleOutline />
+        </button>
+        <button>
+          <IoPlayForwardSharp />
+        </button>
+        <button className="text-base">
+          <FaRegHeart />
+        </button>
+      </div>
       <button className="lg:hidden">
         <HiMenu />
       </button>
@@ -17,8 +41,7 @@ export const Header = () => {
           <IoPersonCircle />
         </a>
       ) : null}
-
-      <div className="absolute left-1/2 h-full -translate-x-1/2 p-3">
+      <div className="absolute left-1/2 top-0 bottom-0 flex h-full -translate-x-1/2 items-center p-3">
         <img className="block" src={Logo} alt="Las.fm logo" />
       </div>
       <button
